e6efd2c07a3295ad1e4e96a6dd4e068f09587535
Eran Usha <eusha5@gmail.com>
2018-11-09 14:24:24 -0500

    created child theme

---
+++ wp-content/themes/wordcamp-orl2018/functions.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Wordcamp Orlando Demo
+ * 
+ * @package wordcamp-orl2018
+ */
+
+/**
+* Load files in the /assets/ directory
+*
+* @since 1.0.0
+*
+*/
+
+function child_theme_load_assets() {
+  //Load the main stylesheet first
+  if(is_child_theme()) {
+    wp_enqueue_style('parent-styles', trailingslashit(get_template_directory_uri()).'style.css');
+  }
+
+  //Load the main CSS file
+  //wp_enqueue_style('child-styles', get_stylesheet_directory_uri().'/css/main.css');
+  wp_enqueue_style('child-styles', get_stylesheet_directory_uri());
+}
+add_action('wp_enqueue_scripts', 'child_theme_load_assets', 1);
